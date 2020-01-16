3bb0882a02dc1e66b51842bfebc2ba8eaa6261c5
tao.liao@m2micro.com <lt374268>
2020-01-16 11:40:33 +0800

    logback

--- src/test/java/com/example/redis/overrite/LedisTest.java
+++ src/test/java/com/example/redis/overrite/LedisTest.java
@@ -1,15 +1,18 @@
 package com.example.redis.overrite;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class LedisTest {
     @Test
     public void test(){
-
+        log.debug("dd");
         Ledis ledis = new Ledis("127.0.0.1",6379);
         ledis.set("ces","333333");
+
     }
 
 }
